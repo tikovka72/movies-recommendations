5fc8f2d882d1bfe0a10121fb19a3b19896218930
Tikovka72 <zeytunyan2004@mail.ru>
2022-03-24 22:02:35 +0500

    column's list and typing added. small refactor

--- csv_parse.py
+++ csv_parse.py
@@ -2,15 +2,17 @@ import pandas as pd
 from json import loads
 import numpy as np
 from pprint import pprint
+from typing import Sized
 
 df = pd.read_csv('csv/movies_raw.csv')
 
 
-def drop(dataframe, columns):
-    return dataframe.drop(columns=columns)
+def drop(dataframe: pd.DataFrame,
+         columns_to_drop: Sized) -> pd.DataFrame:
+    return dataframe.drop(columns=columns_to_drop)
 
 
-def apply_genres(dataframe):
+def apply_genres(dataframe: pd.DataFrame) -> (pd.DataFrame, tuple):
     unique_genres = set()
 
     dataframe['genres'] = tuple(map(
@@ -27,11 +29,11 @@ def apply_genres(dataframe):
     for i in unique_genres:
         dataframe.loc[
             dataframe['genres'].apply(lambda x: i in x), i] = 1
-    dataframe.drop(columns='genres')
-    return dataframe
+    dataframe.drop(columns_to_drop='genres')
+    return dataframe, tuple(unique_genres)
 
 
-def apply_keywords(dataframe):
+def apply_keywords(dataframe: pd.DataFrame) -> pd.DataFrame:
     unique_keywords = set()
 
     dataframe['keywords'] = tuple(map(
@@ -41,9 +43,11 @@ def apply_keywords(dataframe):
                 loads(jsons))),
         tuple(dataframe['keywords'])))
     print(len(unique_keywords), unique_keywords)
+    return dataframe
 
 
-def apply_production_companies(dataframe):
+def apply_production_countries(
+        dataframe: pd.DataFrame) -> (pd.DataFrame, tuple):
     unique_production_countries = set()
 
     dataframe['production_countries'] = tuple(map(
@@ -62,12 +66,12 @@ def apply_production_companies(dataframe):
         dataframe.loc[
             dataframe['production_countries'].apply(lambda x: i in x), i] = 1
         if dataframe[i].sum() < 2:
-            dataframe.drop(columns=i)
-    dataframe.drop(columns='production_countries')
-    return dataframe
+            dataframe.drop(columns_to_drop=i)
+    dataframe.drop(columns_to_drop='production_countries')
+    return dataframe, tuple(unique_production_countries)
 
 
-def apply_release_date(dataframe):
+def apply_release_date(dataframe: pd.DataFrame) -> pd.DataFrame:
     def apply(x):
         return int(x.split('-')[0])
 
@@ -82,11 +86,18 @@ df = drop(df, ['homepage', 'id', 'budget', 'original_language',
                'revenue', 'original_title',
                ])
 
-df = apply_genres(df)
+columns = []
+
+df, genres = apply_genres(df)
+columns += genres
+
 # TODO разобраться с ключевыми словами
 # df = apply_keywords(df)
-df = apply_production_companies(df)
+df, production_countries = apply_production_countries(df)
+columns += production_countries
+
 df = apply_release_date(df)
 df = df[df['status'] == 'Released']
+
 df.to_csv('csv/movies.csv')
 

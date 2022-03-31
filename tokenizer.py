2c91d1ef6f8b9bcfc767b163da31e93c2a6de73b
Tikovka72 <zeytunyan2004@mail.ru>
2022-03-31 15:28:57 +0500

    tokenizer rewritten as class

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,26 @@
+import numpy as np
 from pickle import load
+from typing import Iterable
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 
-def get_tokenizer(fp='tokenizer.pickle'):
-    with open(fp, 'rb') as f:
-        tokenizer = load(f)
+class CustomTokenizer:
+    def __init__(self, fp='tokenizer.pickle'):
+        with open(fp, 'rb') as f:
+            self.tokenizer: Tokenizer = load(f)
 
-    return tokenizer
+    @staticmethod
+    def _indexes_to_binary(
+            words_indexes: np.array,
+            bag_size: int = 8000):
+        if not bag_size:
+            bag_size = max(words_indexes)
+        binary_words = np.zeros(bag_size)
+        binary_words[words_indexes] = 1
+        return binary_words
+
+    def _get_indexes(self, words: Iterable):
+        return self.tokenizer.texts_to_sequences(words)
+
+    def get_binary_indexes(self, words: Iterable):
+        return self._indexes_to_binary(self._get_indexes(words))
--- utils.py
+++
@@ -1,4 +0,0 @@
-def keywords_to_binary(words_indexes, num_words=8000):
-    binary_words = np.zeros(num_words)
-    binary_words[words_indexes] = 1
-    return binary_words

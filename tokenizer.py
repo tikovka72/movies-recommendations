c3adf8f9fd794726930a0066fe3fcc6ed2f271fc
Tikovka72 <zeytunyan2004@mail.ru>
2022-03-31 16:05:37 +0500

    methods for getting indexes and for cutting words in tokenizer added

--- tokenizer.py
+++ tokenizer.py
@@ -5,22 +5,28 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 class CustomTokenizer:
-    def __init__(self, fp='tokenizer.pickle'):
+    def __init__(self, fp: str = 'tokenizer.pickle',
+                 splitter: str = ';',
+                 bag_size: int = 8000):
         with open(fp, 'rb') as f:
             self.tokenizer: Tokenizer = load(f)
 
-    @staticmethod
-    def _indexes_to_binary(
-            words_indexes: np.array,
-            bag_size: int = 8000):
-        if not bag_size:
-            bag_size = max(words_indexes)
-        binary_words = np.zeros(bag_size)
+        self.splitter = splitter
+        self.bag_size = bag_size
+
+    def _indexes_to_binary(self, words_indexes: np.array) -> np.array:
+        binary_words = np.zeros(self.bag_size if self.bag_size
+                                else max(words_indexes))
+
         binary_words[words_indexes] = 1
         return binary_words
 
-    def _get_indexes(self, words: Iterable):
-        return self.tokenizer.texts_to_sequences(words)
+    def _get_indexes(self, words: Iterable) -> np.array:
+        return np.array(self.tokenizer.texts_to_sequences(words)
+                        ).flatten()
 
-    def get_binary_indexes(self, words: Iterable):
+    def get_binary_indexes(self, words: Iterable) -> np.array:
         return self._indexes_to_binary(self._get_indexes(words))
+
+    def cut_words(self, words: str) -> list:
+        return words.split(self.splitter)

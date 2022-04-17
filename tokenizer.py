9bd385e6598710f825785abb2ce39e6b9f01c514
Tikovka72 <zeytunyan2004@mail.ru>
2022-04-17 15:09:09 +0500

    similar words added

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,16 @@
+from difflib import SequenceMatcher
+
 import numpy as np
 from pickle import load
 from typing import Iterable
+
+import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 class CustomTokenizer:
+    MIN_RATIO = 0.8
+
     def __init__(self, fp: str = 'tokenizer.pickle',
                  splitter: str = ';',
                  bag_size: int = 8000):
@@ -13,6 +19,12 @@ class CustomTokenizer:
 
         self.splitter = splitter
         self.bag_size = bag_size
+
+        all_words = [''] + list(map(lambda x: x[1], sorted(
+            tuple(self.tokenizer.index_word.items()),
+            key=lambda x: x[0])))
+
+        self.all_words = np.array(all_words, dtype=str)
 
     def _indexes_to_binary(self, words_indexes: np.array) -> np.array:
         binary_words = np.zeros(self.bag_size if self.bag_size
@@ -26,11 +38,52 @@ class CustomTokenizer:
             tuple(filter(
                 lambda index: index != -1,
                 map(lambda w: w[0] if len(w) else -1,
-                    self.tokenizer.texts_to_sequences(words)))))\
+                    self.tokenizer.texts_to_sequences(words))))) \
             .flatten().astype(int)
 
-    def get_binary_indexes(self, words: Iterable) -> np.array:
-        return self._indexes_to_binary(self._get_indexes(words))
+    def get_binary_indexes(self, words: Iterable,
+                           need_similar: bool = True) -> np.array:
+        indexes = self._get_indexes(words)
+        if need_similar:
+            indexes = self._get_similar_words(indexes)
+        return self._indexes_to_binary(indexes)
 
     def cut_words(self, words: str) -> list:
         return words.split(self.splitter)
+
+    def _get_similar_words(self, indexes) -> np.array:
+        similar_words = np.zeros(self.bag_size)
+
+        for index in indexes:
+            similar_words += \
+                np.array(
+                    [int(SequenceMatcher(
+                        None, self.all_words[index],
+                        self.all_words[temp_index])
+                         .ratio() > self.MIN_RATIO)
+
+                     for temp_index in range(self.bag_size)])
+        return np.where(similar_words > 0)
+
+
+def testing_similar_words():
+    df = pd.read_csv('csv/movies.csv')
+    keywords = df['keywords'].dropna()
+    my_tokenizer = CustomTokenizer()
+    keywords = my_tokenizer.cut_words(keywords[0])
+    indexes1 = np.sort(my_tokenizer._get_indexes(keywords))
+    indexes2 = np.sort(
+        np.where(my_tokenizer.get_binary_indexes(keywords, True) > 0)[0])
+    max_len_indexes1 = len(my_tokenizer.tokenizer.index_word[
+        max(indexes1,
+            key=lambda x: len(my_tokenizer.tokenizer.index_word[x]))])
+
+    for i in indexes1:
+        word = my_tokenizer.tokenizer.index_word[i]
+        print(word, ' ' * (max_len_indexes1 - len(word)), word)
+
+    for i in indexes2:
+        if i in indexes1:
+            continue
+        print(' ' * (max_len_indexes1 + 1),
+              my_tokenizer.tokenizer.index_word[i].strip())
